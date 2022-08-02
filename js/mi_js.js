d4a19a3286657020a4fe53bd59c397d814ae3918
Rafael Soriano <rsoriano.insti@gmail.com>
2022-08-02 17:27:07 +0200

    2

--- js/mi_js.js
+++ js/mi_js.js
@@ -1,41 +1,4 @@
-/*
-    document.addEventListener('DOMContentLoaded', function () {
-
-    // Bloqueamos los desplegables de Talla y Ubicación, y todos los botones 
-    document.getElementById("desplegableTalla").disabled=true;
-    //document.getElementById("desplegableUbicacion").disabled=true;
-    document.getElementById("botonAgregarACesta").disabled=true;
-    
-    // Cargamos los valores del desplegable Prendas
-    cargarDesplegablePrendas();
-    
-});
-
-    document.getElementById("desplegablePrenda").addEventListener('change', function () {})
-
-<script>
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-</script>
-
-
-*/
-
 function expand(element) {
-    element.style.fontWeight = 'bold';
-    element.nextElementSibling.style.height = 'auto';
+  element.classList.toggle("active");
+  element.nextElementSibling.classList.toggle("active");
 }
-
-//
